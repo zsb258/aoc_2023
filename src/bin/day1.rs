9f4ce7a1e41b292745973bed8d19707d5cd15890
zsb258 <zhuang.shengbin@u.nus.edu>
2023-12-01 22:23:29 +0800

    day1 refactor

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,15 +1,15 @@
 const INPUT: &str = include_str!("../../inputs/day1.txt");
 
 fn main() {
-    println!("Part1: {}", part1());
-    println!("Part2: {}", part2());
+    println!("Part1: {}", solve_with(&part1_parse_digits));
+    println!("Part2: {}", solve_with(&part2_parse_alpha_digits));
 }
 
-fn part1() -> u32 {
+fn solve_with(parser: &'_ dyn Fn(&str) -> Box<dyn Iterator<Item = u32> + '_>) -> u32 {
     INPUT
         .lines()
         .map(|line| {
-            let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+            let mut digits = parser(line);
             let first = digits.next().unwrap();
             let last = digits.last().unwrap_or(first);
             first * 10 + last
@@ -17,48 +17,35 @@ fn part1() -> u32 {
         .sum()
 }
 
-fn part2() -> u32 {
-    INPUT
-        .lines()
-        .map(|line| {
-            let digits = parse_alpha_digits(line);
-            let first = digits.first().unwrap();
-            let last = digits.last().unwrap_or(first);
-            first * 10 + last
-        })
-        .sum()
+fn part1_parse_digits(line: &str) -> Box<dyn Iterator<Item = u32> + '_> {
+    Box::new(line.chars().filter_map(|c| c.to_digit(10)))
 }
 
-fn parse_alpha_digits(line: &str) -> Vec<u32> {
-    let mut i = 0;
-    let mut digits = Vec::new();
-    while i < line.len() {
-        if let Some(d) = line[i..].chars().next().unwrap().to_digit(10) {
-            digits.push(d);
-            i += 1;
-            continue;
-        }
+fn part2_parse_alpha_digits(line: &str) -> Box<dyn Iterator<Item = u32> + '_> {
+    Box::new((0..line.len()).filter_map(|i| {
+        if let Some(n) = line[i..].chars().next().unwrap().to_digit(10) {
+            Some(n)
+        } else {
+            let mut j = i + 2;
 
-        let mut j = i + 2;
-
-        while j < line.len() && j < i + 5 {
-            match line[i..=j].to_string().as_str() {
-                "one" => digits.push(1),
-                "two" => digits.push(2),
-                "three" => digits.push(3),
-                "four" => digits.push(4),
-                "five" => digits.push(5),
-                "six" => digits.push(6),
-                "seven" => digits.push(7),
-                "eight" => digits.push(8),
-                "nine" => digits.push(9),
-                _ => (),
+            while j < line.len() && j < i + 5 {
+                if let Some(n) = match line[i..=j].to_string().as_str() {
+                    "one" => Some(1),
+                    "two" => Some(2),
+                    "three" => Some(3),
+                    "four" => Some(4),
+                    "five" => Some(5),
+                    "six" => Some(6),
+                    "seven" => Some(7),
+                    "eight" => Some(8),
+                    "nine" => Some(9),
+                    _ => None,
+                } {
+                    return Some(n);
+                }
+                j += 1;
             }
-            j += 1;
+            None
         }
-
-        i += 1;
-    }
-
-    digits
+    }))
 }
