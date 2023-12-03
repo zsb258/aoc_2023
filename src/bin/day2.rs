6091819d570a9ab6d71ecd1d772909f83613ff9f
zsb258 <zhuang.shengbin@u.nus.edu>
2023-12-03 10:35:49 +0800

    day2 refactor

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,122 +1,90 @@
 use std::collections::HashMap;
 
-const INPUT: &str = include_str!("../../inputs/day2.txt");
-// const INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-
-const MAX_R: i32 = 12;
-const MAX_G: i32 = 13;
-const MAX_B: i32 = 14;
-
 fn main() {
-    println!("Part1: {}", part1());
-    println!("Part2: {}", part2());
+    let input: &str = include_str!("../../inputs/day2.txt");
+    println!("Part1: {}", solve_with(input, part1));
+    println!("Part2: {}", solve_with(input, part2));
 }
 
-fn part1() -> i32 {
-    INPUT
+fn solve_with(input: &str, solver: impl Fn(Game) -> Option<u32>) -> u32 {
+    input
         .lines()
         .filter_map(|line| {
             let game = parse_line(line);
-            let r = game
-                .draws
-                .iter()
-                .map(|draw| draw.get(&'r').unwrap_or(&0))
-                .max()
-                .unwrap();
-
-            let g = game
-                .draws
-                .iter()
-                .map(|draw| draw.get(&'g').unwrap_or(&0))
-                .max()
-                .unwrap();
-
-            let b = game
-                .draws
-                .iter()
-                .map(|draw| draw.get(&'b').unwrap_or(&0))
-                .max()
-                .unwrap();
-
-            if r <= &MAX_R && g <= &MAX_G && b <= &MAX_B {
-                Some(game.id)
-            } else {
-                None
-            }
+            solver(game)
         })
         .sum()
 }
 
-fn part2() -> i32 {
-    INPUT
-        .lines()
-        .filter_map(|line| {
-            let game = parse_line(line);
-            let r = game
-                .draws
-                .iter()
-                .map(|draw| draw.get(&'r').unwrap_or(&0))
-                .max()
-                .unwrap();
+fn part1(game: Game) -> Option<u32> {
+    const MAX_R: u32 = 12;
+    const MAX_G: u32 = 13;
+    const MAX_B: u32 = 14;
 
-            let g = game
-                .draws
-                .iter()
-                .map(|draw| draw.get(&'g').unwrap_or(&0))
-                .max()
-                .unwrap();
+    if game.r <= MAX_R && game.g <= MAX_G && game.b <= MAX_B {
+        Some(game.id)
+    } else {
+        None
+    }
+}
 
-            let b = game
-                .draws
-                .iter()
-                .map(|draw| draw.get(&'b').unwrap_or(&0))
-                .max()
-                .unwrap();
-
-            Some(r * g * b)
-        })
-        .sum()
+fn part2(game: Game) -> Option<u32> {
+    Some(game.r * game.g * game.b)
 }
 
 struct Game {
-    id: i32,
-    draws: Vec<HashMap<char, i32>>,
+    id: u32,
+    r: u32,
+    g: u32,
+    b: u32,
 }
 
 fn parse_line(line: &str) -> Game {
-    let mut line_splits = line.split(": ");
-    let id = line_splits
-        .next()
-        .unwrap()
-        .split(' ')
-        .last()
-        .unwrap()
-        .parse::<i32>()
-        .unwrap();
+    let (id_str, draws_str) = match line.splitn(2, ": ").collect::<Vec<_>>()[..] {
+        [id_str, draws_str] => (id_str, draws_str),
+        _ => panic!("Invalid line format"),
+    };
 
-    let draws = line_splits
-        .next()
-        .unwrap()
+    let id = id_str.split(' ').last().unwrap().parse::<u32>().unwrap();
+
+    let draws = draws_str
         .split("; ")
         .map(|draw_str| {
             draw_str
                 .split(", ")
-                .map(|draw| {
-                    let mut draw_splits = draw.split(' ');
-                    let count = draw_splits.next().unwrap().parse::<i32>().unwrap();
-                    let color = draw_splits.next().unwrap().chars().next().unwrap();
-                    assert!(draw_splits.next().is_none());
-                    (color, count)
+                .map(|draw| match draw.splitn(2, ' ').collect::<Vec<_>>()[..] {
+                    [count, color] => {
+                        (color.chars().next().unwrap(), count.parse::<u32>().unwrap())
+                    }
+                    _ => panic!("Invalid draw format"),
                 })
                 .collect::<HashMap<_, _>>()
         })
         .collect::<Vec<_>>();
 
-    assert!(line_splits.next().is_none());
+    let mut max_vals = ['r', 'g', 'b'].iter().map(|color| {
+        draws
+            .iter()
+            .map(|draw| *draw.get(color).unwrap_or(&0))
+            .max()
+            .unwrap()
+    });
 
-    Game { id, draws }
+    Game {
+        id,
+        r: max_vals.next().unwrap(),
+        g: max_vals.next().unwrap(),
+        b: max_vals.next().unwrap(),
+    }
+}
+
+#[test]
+fn example() {
+    let example: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    assert_eq!(solve_with(example, part1), 8);
+    assert_eq!(solve_with(example, part2), 2286);
 }
