a52f49fd8552a182429a3a17f27d6119bb4ce724
zsb258 <zhuang.shengbin@u.nus.edu>
2023-12-03 17:50:34 +0800

    day3 refactor

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -2,11 +2,41 @@ use std::collections::VecDeque;
 
 fn main() {
     let input: &str = include_str!("../../inputs/day3.txt");
-    println!("Part1: {}", part1(input));
-    println!("Part2: {}", part2(input));
+    println!(
+        "Part1: {}",
+        solve_with(input, &part1_symbol_predicate, &part1_agg_fn)
+    );
+    println!(
+        "Part2: {}",
+        solve_with(input, &part2_symbol_predicate, &part2_agg_fn)
+    );
 }
 
-fn part1(input: &str) -> u32 {
+fn part1_symbol_predicate(c: &char) -> bool {
+    !c.is_ascii_digit() && *c != '.'
+}
+
+fn part1_agg_fn(numbers: Vec<u32>) -> Option<u32> {
+    Some(numbers.iter().sum())
+}
+
+fn part2_symbol_predicate(c: &char) -> bool {
+    *c == '*'
+}
+
+fn part2_agg_fn(numbers: Vec<u32>) -> Option<u32> {
+    if numbers.len() == 2 {
+        Some(numbers[0] * numbers[1])
+    } else {
+        None
+    }
+}
+
+fn solve_with(
+    input: &str,
+    symbol_pred: &impl Fn(&char) -> bool,
+    agg_fn: &impl Fn(Vec<u32>) -> Option<u32>,
+) -> u32 {
     let grid: Vec<Vec<char>> = input
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
@@ -21,69 +51,13 @@ fn part1(input: &str) -> u32 {
             row.iter()
                 .enumerate()
                 .filter_map(move |(j, elem)| match elem {
-                    c if !c.is_ascii_digit() && c != &'.' => Some((i, j)),
-                    _ => None,
-                })
-        })
-        .map(|(i, j)| {
-            // dbg!((i, j));
-            let mut numbers = Vec::new();
-            for (r, c) in adjacent(i, j, n, m) {
-                // dbg!((r, c));
-                if !seen[r][c] && grid[r][c].is_ascii_digit() {
-                    let mut digits: VecDeque<_> =
-                        VecDeque::from([grid[r][c].to_digit(10).unwrap()]);
-                    seen[r][c] = true;
-
-                    // check left
-                    let mut left = 1;
-                    while c >= left && grid[r][c - left].is_ascii_digit() {
-                        digits.push_front(grid[r][c - left].to_digit(10).unwrap());
-                        seen[r][c - left] = true;
-                        left += 1;
-                    }
-
-                    // check right
-                    let mut right = 1;
-                    while c + right < m && grid[r][c + right].is_ascii_digit() {
-                        digits.push_back(grid[r][c + right].to_digit(10).unwrap());
-                        seen[r][c + right] = true;
-                        right += 1;
-                    }
-
-                    numbers.push(digits.iter().fold(0, |acc, x| acc * 10 + x));
-                }
-            }
-            // dbg!(&numbers);
-            numbers.iter().sum::<u32>()
-        })
-        .sum()
-}
-
-fn part2(input: &str) -> u32 {
-    let grid: Vec<Vec<char>> = input
-        .lines()
-        .map(|line| line.chars().collect::<Vec<char>>())
-        .collect();
-    let n: usize = grid.len();
-    let m: usize = grid[0].len();
-    let mut seen = vec![vec![false; m]; n];
-
-    grid.iter()
-        .enumerate()
-        .flat_map(|(i, row)| {
-            row.iter()
-                .enumerate()
-                .filter_map(move |(j, elem)| match elem {
-                    '*' => Some((i, j)),
+                    c if symbol_pred(c) => Some((i, j)),
                     _ => None,
                 })
         })
         .filter_map(|(i, j)| {
-            // dbg!((i, j));
             let mut numbers = Vec::new();
             for (r, c) in adjacent(i, j, n, m) {
-                // dbg!((r, c));
                 if !seen[r][c] && grid[r][c].is_ascii_digit() {
                     let mut digits: VecDeque<_> =
                         VecDeque::from([grid[r][c].to_digit(10).unwrap()]);
@@ -108,12 +82,7 @@ fn part2(input: &str) -> u32 {
                     numbers.push(digits.iter().fold(0, |acc, x| acc * 10 + x));
                 }
             }
-            // dbg!(&numbers);
-            if numbers.len() == 2 {
-                Some(numbers[0] * numbers[1])
-            } else {
-                None
-            }
+            agg_fn(numbers)
         })
         .sum()
 }
@@ -155,6 +124,26 @@ fn example() {
 ......755.
 ...$.*....
 .664.598..";
-    assert_eq!(part1(example), 4361);
-    assert_eq!(part2(example), 467835);
+    assert_eq!(
+        solve_with(example, &part1_symbol_predicate, &part1_agg_fn),
+        4361
+    );
+    assert_eq!(
+        solve_with(example, &part2_symbol_predicate, &part2_agg_fn),
+        467835
+    );
+}
+
+#[test]
+/// for refractoring
+fn answer() {
+    let input: &str = include_str!("../../inputs/day3.txt");
+    assert_eq!(
+        solve_with(input, &part1_symbol_predicate, &part1_agg_fn),
+        525911
+    );
+    assert_eq!(
+        solve_with(input, &part2_symbol_predicate, &part2_agg_fn),
+        75805607
+    );
 }
