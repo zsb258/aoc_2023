fbdc4fd75344b84d72137e313e9d3801732b41e5
zsb258 <zhuang.shengbin@u.nus.edu>
2023-12-05 17:27:47 +0800

    day5 refactor

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,58 +1,20 @@
 fn main() {
     let input: &str = include_str!("../../inputs/day5.txt");
-    println!("Part1: {}", part1(input));
-    println!("Part2: {}", part2(input));
+    println!("Part1: {}", solve_with(input, &part1_seeds_parser));
+    println!("Part2: {}", solve_with(input, &part2_seeds_parser));
 }
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(Debug)]
 struct Mapper {
     dst_start: u64,
     src_start: u64,
     length: u64,
 }
 
-impl PartialOrd for Mapper {
-    fn partial_cmp(&self, other: &Mapper) -> Option<std::cmp::Ordering> {
-        Some(self.src_start.cmp(&other.src_start))
-    }
-}
-
-impl Ord for Mapper {
-    fn cmp(&self, other: &Mapper) -> std::cmp::Ordering {
-        self.src_start.cmp(&other.src_start)
-    }
-}
-
-/// brute force
-fn map_all(src: Vec<u64>, mappers: Vec<Mapper>) -> Vec<u64> {
-    let mut dst = Vec::new();
-
-    for &s in src.iter() {
-        if let Some(m) = mappers
-            .iter()
-            .find(|m| s >= m.src_start && s < m.src_start + m.length)
-        {
-            dst.push(m.dst_start + s - m.src_start);
-        } else {
-            dst.push(s);
-        }
-    }
-
-    dst
-}
-
+/// src and mappers vectors are sorted by start position
 fn map_all_range(src: Vec<(u64, u64)>, mappers: Vec<Mapper>) -> Vec<(u64, u64)> {
-    let mut src = src;
-    src.sort();
-
-    let mut mappers = mappers;
-    mappers.sort();
-
     let mut dst = Vec::new();
     let mut j = 0;
-
-    dbg!(&src);
-    dbg!(&mappers);
 
     for s in src {
         let end = s.0 + s.1;
@@ -75,18 +37,13 @@ fn map_all_range(src: Vec<(u64, u64)>, mappers: Vec<Mapper>) -> Vec<(u64, u64)> 
                     let delta = m.src_start + m.length - curr;
                     dst.push((m.dst_start + curr - m.src_start, delta));
                     curr += delta;
-                    if j < mappers.len() - 1 {
-                        j += 1;
-                    }
                 }
+            } else if end >= m.src_start {
+                dst.push((curr, m.src_start - curr));
+                curr = m.src_start;
             } else {
-                if end >= m.src_start {
-                    dst.push((curr, m.src_start - curr));
-                    curr = m.src_start;
-                } else {
-                    dst.push((curr, end - curr));
-                    break;
-                }
+                dst.push((curr, end - curr));
+                break;
             }
         }
     }
@@ -94,70 +51,28 @@ fn map_all_range(src: Vec<(u64, u64)>, mappers: Vec<Mapper>) -> Vec<(u64, u64)> 
     dst
 }
 
-fn part1(input: &str) -> u64 {
-    let mut splits1 = input.splitn(2, '\n');
-    let seeds = splits1
-        .next()
-        .unwrap()
-        .split(": ")
-        .nth(1)
-        .unwrap()
+fn part1_seeds_parser(input: &str) -> Vec<(u64, u64)> {
+    input
         .split_whitespace()
-        .map(|x| x.parse::<u64>().unwrap())
-        .collect::<Vec<_>>();
-
-    let mappers_lst = splits1
-        .next()
-        .unwrap()
-        .split("\n\n")
-        .map(|s| {
-            s.split(":\n")
-                .nth(1)
-                .unwrap()
-                .lines()
-                .map(|line| {
-                    if let [dst_start, src_start, length] = line
-                        .split_whitespace()
-                        .map(|x| x.parse::<u64>().unwrap())
-                        .collect::<Vec<_>>()[..]
-                    {
-                        Mapper {
-                            dst_start,
-                            src_start,
-                            length,
-                        }
-                    } else {
-                        panic!("invalid input")
-                    }
-                })
-                .collect::<Vec<Mapper>>()
-        })
-        .collect::<Vec<Vec<Mapper>>>();
-
-    let mut src = seeds;
-    for mappers in mappers_lst {
-        src = map_all(src, mappers);
-    }
-
-    *src.iter().min().unwrap()
+        .map(|x| (x.parse::<u64>().unwrap(), 1_u64))
+        .collect::<Vec<_>>()
 }
 
-fn part2(input: &str) -> u64 {
-    let mut splits1 = input.splitn(2, '\n');
-    let seeds = splits1
-        .next()
-        .unwrap()
-        .split(": ")
-        .nth(1)
-        .unwrap()
+fn part2_seeds_parser(input: &str) -> Vec<(u64, u64)> {
+    input
         .split_whitespace()
         .map(|x| x.parse::<u64>().unwrap())
         .collect::<Vec<_>>()[..]
         .chunks(2)
         .map(|pair| (pair[0], pair[1]))
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
 
-    let mappers_lst = splits1
+fn solve_with(input: &str, seeds_parser: &'_ dyn Fn(&'_ str) -> Vec<(u64, u64)>) -> u64 {
+    let mut splits = input.splitn(2, '\n');
+    let seeds = seeds_parser(splits.next().unwrap().split(": ").nth(1).unwrap());
+
+    splits
         .next()
         .unwrap()
         .split("\n\n")
@@ -183,19 +98,15 @@ fn part2(input: &str) -> u64 {
                 })
                 .collect::<Vec<Mapper>>()
         })
-        .collect::<Vec<Vec<Mapper>>>();
-
-    dbg!(&seeds);
-    dbg!(&mappers_lst);
-
-    let mut src = seeds;
-    for mappers in mappers_lst {
-        src = map_all_range(src, mappers);
-        dbg!(&src);
-        // panic!("dbg");
-    }
-
-    src.iter().min().unwrap().0
+        .fold(seeds, |mut src, mut mappers| {
+            src.sort();
+            mappers.sort_by(|a, b| a.src_start.cmp(&b.src_start));
+            map_all_range(src, mappers)
+        })
+        .iter()
+        .min()
+        .unwrap()
+        .0
 }
 
 #[test]
@@ -233,6 +144,13 @@ temperature-to-humidity map:
 humidity-to-location map:
 60 56 37
 56 93 4";
-    assert_eq!(part1(example), 35);
-    assert_eq!(part2(example), 46);
+    assert_eq!(solve_with(example, &part1_seeds_parser), 35);
+    assert_eq!(solve_with(example, &part2_seeds_parser), 46);
+}
+
+#[test]
+fn answer() {
+    let input: &str = include_str!("../../inputs/day5.txt");
+    assert_eq!(solve_with(input, &part1_seeds_parser), 84470622);
+    assert_eq!(solve_with(input, &part2_seeds_parser), 26714516);
 }
