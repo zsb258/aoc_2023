e4080c6d3c19e2b340a4bd79c4c91bf6bd48fb54
zsb258 <zhuang.shengbin@u.nus.edu>
2023-12-07 16:07:17 +0800

    day7 refactor

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,16 +1,6 @@
-fn main() {
-    let input: &str = include_str!("../../inputs/day7.txt");
-    println!("Part1: {}", part1(input));
-    println!("Part2: {}", part2(input));
+use std::collections::HashMap;
 
-    let example: &str = "32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483";
-    println!("{}", part1(example));
-    println!("{}", part2(example));
-}
+use itertools::Itertools;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Hand {
@@ -49,123 +39,73 @@ enum HandType {
 }
 
 impl HandType {
-    fn _new(cards: &[u8]) -> Self {
-        use HandType as H;
-        let mut counts = [0; 14 + 1];
-        for &card in cards {
-            counts[card as usize] += 1;
-        }
-        let mut counts = counts.iter().filter(|&&n| n > 0).collect::<Vec<_>>();
-        counts.sort();
-
-        dbg!(&counts);
-
-        match counts.last().unwrap() {
-            5 => H::FiveOfAKind,
-            4 => H::FourOfAKind,
-            3 => {
-                if counts.len() == 2 {
-                    H::FullHouse
-                } else {
-                    H::ThreeOfAKind
-                }
-            }
-            2 => {
-                if counts.len() == 3 {
-                    H::TwoPair
-                } else {
-                    H::OnePair
-                }
-            }
-            _ => H::HighCard,
-        }
-    }
-
     fn new(cards: &[u8]) -> Self {
-        use HandType as H;
-        let mut counts = [0; 14 + 1];
-        let mut jokers = 0;
-        for &card in cards {
-            if card == 1 {
-                jokers += 1;
-            } else {
-                counts[card as usize] += 1;
-            }
-        }
+        let jokers = cards.iter().filter(|&&n| n == 1).count();
 
-        let mut counts = counts.iter().filter(|&&n| n > 0).collect::<Vec<_>>();
-        counts.sort();
+        let counts = cards
+            .iter()
+            .filter(|&&n| n != 1)
+            .fold(HashMap::new(), |mut acc, &card| {
+                *acc.entry(card).or_insert(0) += 1;
+                acc
+            })
+            .iter()
+            .map(|(_, &n)| n)
+            .sorted()
+            .collect::<Vec<_>>();
 
-        dbg!(&cards);
-        dbg!(&counts, jokers);
-
-        let highest = *counts.last().unwrap_or(&&0) + jokers;
-        let unique_count = counts.len() + if jokers > 0 { 1 } else { 0 };
-        dbg!(highest, unique_count);
-
-        match highest {
-            5 => H::FiveOfAKind,
-            4 => H::FourOfAKind,
-            3 => match jokers {
-                0 => match counts[..] {
-                    [2, 3] => H::FullHouse,
-                    [1, 1, 3] => H::ThreeOfAKind,
-                    _ => unreachable!(),
-                },
-                1 => match counts[..] {
-                    [2, 2] => H::FullHouse,
-                    [1, 1, 2] => H::ThreeOfAKind,
-                    _ => unreachable!(),
-                },
-                2 => match counts[..] {
-                    [1, 1, 1] => H::ThreeOfAKind,
-                    _ => unreachable!(),
-                },
-                3 => match counts[..] {
-                    [1, 1, 1] => H::ThreeOfAKind,
-                    _ => unreachable!(),
-                },
-                _ => unreachable!(),
-            },
-            2 => match jokers {
-                0 => match counts[..] {
-                    [1, 2, 2] => H::TwoPair,
-                    [1, 1, 1, 2] => H::OnePair,
-                    _ => unreachable!(),
-                },
-                1 => match counts[..] {
-                    [1, 1, 1, 1] => H::OnePair,
-                    _ => unreachable!(),
-                },
-                2 => match counts[..] {
-                    [1, 1, 1] => H::OnePair,
-                    _ => unreachable!(),
-                },
-                _ => unreachable!(),
-            },
-            _ => H::HighCard,
+        match (counts.last().unwrap_or(&0) + jokers, counts.len()) {
+            (5, _) => Self::FiveOfAKind,
+            (4, _) => Self::FourOfAKind,
+            (3, 2) => Self::FullHouse,
+            (3, 3) => Self::ThreeOfAKind,
+            (2, 3) => Self::TwoPair,
+            (2, 4) => Self::OnePair,
+            (1, 5) => Self::HighCard,
+            _ => unreachable!(),
         }
     }
 }
 
-fn part1(input: &str) -> u64 {
-    let mut hands = input
+fn main() {
+    let input: &str = include_str!("../../inputs/day7.txt");
+    println!("Part1: {}", solve_with(input, &part1_char_mapper));
+    println!("Part2: {}", solve_with(input, &part2_char_mapper));
+}
+
+fn part1_char_mapper(c: char) -> u8 {
+    match c {
+        'T' => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => c.to_digit(10).unwrap() as u8,
+    }
+}
+
+fn part2_char_mapper(c: char) -> u8 {
+    match c {
+        'T' => 10,
+        'J' => 1, // Joker
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => c.to_digit(10).unwrap() as u8,
+    }
+}
+
+fn solve_with<'a>(input: &'a str, char_mapper: &dyn Fn(char) -> u8) -> u64 {
+    input
         .lines()
-        .map(|line| {
+        .map(|line: &'a str| {
             let mut splits = line.splitn(2, ' ');
 
             let cards = splits
                 .next()
                 .unwrap()
                 .chars()
-                .map(|c| match c {
-                    'T' => 10,
-                    'J' => 11,
-                    'Q' => 12,
-                    'K' => 13,
-                    'A' => 14,
-                    _ => c.to_digit(10).unwrap() as u8,
-                })
+                .map(char_mapper)
                 .collect::<Vec<_>>();
             let hand_type = HandType::new(&cards);
 
@@ -175,25 +115,19 @@ fn part1(input: &str) -> u64 {
                 bid: splits.next().unwrap().parse().unwrap(),
             }
         })
-        .collect::<Vec<_>>();
-    hands.sort();
-
-    hands
-        .iter()
+        .sorted()
         .enumerate()
         .map(|(i, h)| (i as u64 + 1) * h.bid)
         .sum()
 }
 
-fn part2(input: &str) -> u64 {
-    let mut hands = input
-        .lines()
-        .map(|line| {
-            let mut splits = line.splitn(2, ' ');
+#[test]
+fn test_part2_parse_handtype() {
+    use HandType as H;
 
-            let cards = splits
-                .next()
-                .unwrap()
+    fn helper(input: &str) -> H {
+        H::new(
+            &input
                 .chars()
                 .map(|c| match c {
                     'T' => 10,
@@ -203,23 +137,17 @@ fn part2(input: &str) -> u64 {
                     'A' => 14,
                     _ => c.to_digit(10).unwrap() as u8,
                 })
-                .collect::<Vec<_>>();
-            let hand_type = HandType::new(&cards);
+                .collect::<Vec<_>>(),
+        )
+    }
 
-            Hand {
-                cards,
-                hand_type,
-                bid: splits.next().unwrap().parse().unwrap(),
-            }
-        })
-        .collect::<Vec<_>>();
-    hands.sort();
-
-    hands
-        .iter()
-        .enumerate()
-        .map(|(i, h)| (i as u64 + 1) * h.bid)
-        .sum()
+    assert_eq!(helper("3J4QA"), H::OnePair);
+    assert_eq!(helper("3J777"), H::FourOfAKind);
+    assert_eq!(helper("8J6TK"), H::OnePair);
+    assert_eq!(helper("99J9T"), H::FourOfAKind);
+    assert_eq!(helper("9JA4A"), H::ThreeOfAKind);
+    assert_eq!(helper("28J28"), H::FullHouse);
+    assert_eq!(helper("424KT"), H::OnePair);
 }
 
 #[test]
@@ -229,38 +157,13 @@ T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483";
-    assert_eq!(part1(example), 6440);
-    assert_eq!(part2(example), 5905)
+    assert_eq!(solve_with(example, &part1_char_mapper), 6440);
+    assert_eq!(solve_with(example, &part2_char_mapper), 5905);
 }
 
 #[test]
 fn answer() {
     let input: &str = include_str!("../../inputs/day7.txt");
-    assert_eq!(part1(input), 253933213);
-    assert_eq!(part2(input), 253473930);
-}
-
-#[test]
-fn test_handtype_new() {
-    use HandType as H;
-    fn helper(input: &str) -> Vec<u8> {
-        input
-            .chars()
-            .map(|c| match c {
-                'T' => 10,
-                'J' => 1,
-                'Q' => 12,
-                'K' => 13,
-                'A' => 14,
-                _ => c.to_digit(10).unwrap() as u8,
-            })
-            .collect::<Vec<_>>()
-    }
-    assert_eq!(H::new(&helper("3J4QA")), H::OnePair);
-    assert_eq!(H::new(&helper("3J777")), H::FourOfAKind);
-    assert_eq!(H::new(&helper("8J6TK")), H::OnePair);
-    assert_eq!(H::new(&helper("99J9T")), H::FourOfAKind);
-    assert_eq!(H::new(&helper("9JA4A")), H::ThreeOfAKind);
-    assert_eq!(H::new(&helper("28J28")), H::FullHouse);
-    assert_eq!(H::new(&helper("424KT")), H::OnePair);
+    assert_eq!(solve_with(input, &part1_char_mapper), 253933213);
+    assert_eq!(solve_with(input, &part2_char_mapper), 253473930);
 }
